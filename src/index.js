871031c7c81e49cb1294f13c4e40eeccb680e86c
ZoeyF75 <zf.zoodle@gmail.com>
2021-07-30 22:26:32 -0700

    redid setup for heroku

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import Phaser from 'phaser';
+import intro from './scenes/intro';
+import bet from './scenes/bet';
+import deal from './scenes/deal';
+
+class MyGame extends Phaser.Scene
+{
+    constructor ()
+    {
+        super();
+    } 
+}
+
+const config = {
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    width: 800,
+    height: 600,
+    scene: [intro, bet, deal],
+    autoCenter: true
+};
+
+const game = new Phaser.Game(config);
