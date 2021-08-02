1b2466a28745b5fd8f0538861d374f8e1d689243
ZoeyF75 <zf.zoodle@gmail.com>
2021-08-02 11:41:45 -0700

    reset deck

--- src/scenes/intro.js
+++ src/scenes/intro.js
@@ -34,8 +34,8 @@ class intro extends Phaser.Scene {
     });
     this.mysprite.play("animation");
 
-    // const shuffledDeck = createDeck(); //returns array of shuffled deck
-    const shuffledDeck = [11,12,12,3,4,5,6,7,8];
+    const shuffledDeck = createDeck(); //returns array of shuffled deck
+    // const shuffledDeck = [11,12,12,3,4,5,6,7,8];
     this.input.on("pointerdown", () => {
       this.scene.start("bet", {
         balance : 350,
